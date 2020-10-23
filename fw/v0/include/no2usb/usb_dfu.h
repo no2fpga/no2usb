4ec9db6b6b251723bf72c614e198fadeb85588e7
Sylvain Munaut <tnt@246tNt.com>
2020-10-23 18:00:15 +0200

    fw/v0/usb_dfu: Add support for DFU_UPLOAD
    This requires a new callback from the user application
    to read flash.
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- fw/v0/include/no2usb/usb_dfu.h
+++ fw/v0/include/no2usb/usb_dfu.h
@@ -16,6 +16,7 @@ struct usb_dfu_zone {
 void usb_dfu_cb_reboot(void);
 bool usb_dfu_cb_flash_busy(void);
 void usb_dfu_cb_flash_erase(uint32_t addr, unsigned size);			/* 4k, 32k, 64k */
-void usb_dfu_cb_flash_program(const void *data, uint32_t addr, unsigned size);	/* up to 256b   */
+void usb_dfu_cb_flash_program(const void *data, uint32_t addr, unsigned size);	/* up to 256b, page aligned */
+void usb_dfu_cb_flash_read(void *data, uint32_t addr, unsigned size);		/* any addr, any length */
 
 void usb_dfu_init(const struct usb_dfu_zone *zones, int n_zones);
--- fw/v0/src/usb_dfu.c
+++ fw/v0/src/usb_dfu.c
@@ -104,6 +104,7 @@ static struct {
 	uint8_t buf[4096] __attribute__((aligned(4)));
 
 	struct {
+		uint32_t addr_read;
 		uint32_t addr_prog;
 		uint32_t addr_erase;
 		uint32_t addr_end;
@@ -260,8 +261,24 @@ _dfu_ctrl_req(struct usb_ctrl_req *req, struct usb_xfer *xfer)
 		break;
 
 	case USB_RT_DFU_UPLOAD:
-		/* Not supported */
-		goto error;
+		/* Current USB API doesn't have option to defer data, so read
+		 * flash synchronously here, not a big deal since we have
+		 * nothing better to do anyway */
+
+		/* Setup buffer for data */
+		xfer->len  = req->wLength;
+		xfer->data = g_dfu.buf;
+
+		/* Check length doesn't overflow */
+		if ((g_dfu.flash.addr_read + xfer->len) > g_dfu.flash.addr_end)
+			xfer->len = g_dfu.flash.addr_end - g_dfu.flash.addr_read;
+
+		/* Read */
+		if (xfer->len) {
+			usb_dfu_cb_flash_read(xfer->data, g_dfu.flash.addr_read, xfer->len);
+			g_dfu.flash.addr_read += xfer->len;
+		}
+		break;
 
 	case USB_RT_DFU_GETSTATUS:
 		/* Update state */
@@ -329,6 +346,7 @@ _dfu_set_intf(const struct usb_intf_desc *base, const struct usb_intf_desc *sel)
 	g_dfu.intf  = sel->bInterfaceNumber;
 	g_dfu.alt   = sel->bAlternateSetting;
 
+	g_dfu.flash.addr_read  = g_dfu.zones[g_dfu.alt].start;
 	g_dfu.flash.addr_prog  = g_dfu.zones[g_dfu.alt].start;
 	g_dfu.flash.addr_erase = g_dfu.zones[g_dfu.alt].start;
 	g_dfu.flash.addr_end   = g_dfu.zones[g_dfu.alt].end;
